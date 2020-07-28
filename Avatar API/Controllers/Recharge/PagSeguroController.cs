800e0b054ce88ee57cb090e4dc23ee140904bfe2
Vinicius <vinicius.pontespereira@swisscom.com>
2020-07-28 12:32:57 +0200

    adding app settings

---
+++ Avatar API/Controllers/Recharge/PagSeguroController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Avatar_API.Controllers.Recharge
+{
+    public class PagSeguroController
+    {
+    }
+}
